008d2849e4b0c29417390da6c8d931732bbb99ab
jgib <gibersonja@gmail.com>
2014-05-07 13:08:05 -0400

    Update maildrop.pl

--- maildrop.pl
+++ maildrop.pl
@@ -11,8 +11,9 @@ my @get = split("\n",$get);
 
 for(my $i = 0; $i <= $#get; $i++){
         if($get[$i] =~ /<td class=subject><a href="\/inbox\/EMAILNAME\/(.+)">SUBJECT CONTENT HERE<\/a><\/td>/i){
+                                system("curl -X DELETE maildrop.cc/api/inbox/EMAILNAME/$1");
 
-
+          
           
           #########CODE HERE#########
           
